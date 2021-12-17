b12c507f1f548ce9e2fa369c1e93215d42837ad4
Kenny Pflug <kenny.pflug@live.de>
2021-12-17 21:06:18 +0100

    added IsNotNull

--- Code/Light.Validation/Check.cs
+++ Code/Light.Validation/Check.cs
@@ -1,3 +1,7 @@
 ﻿namespace Light.Validation;
 
-public readonly record struct Check<T>(ValidationContext Context, string Key, T Value);
+public readonly record struct Check<T>(ValidationContext Context, string Key, T Value)
+{
+    public void AddError(string errorMessage, bool tryAppend = true) =>
+        Context.AddError(Key, errorMessage, tryAppend);
+}
---
+++ Code/Light.Validation/Checks.cs
@@ -0,0 +1,12 @@
+﻿namespace Light.Validation;
+
+public static class Checks
+{
+    public static Check<T> IsNotNull<T>(this Check<T?> check)
+        where T : class
+    {
+        if (check.Value is null)
+            check.AddError($"{check.Key} must not be null.");
+        return check!;
+    }
+}
