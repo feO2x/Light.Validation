6105f75df5fad2fbd93ba63f832737f528635659
Kenny Pflug <kenny.pflug@live.de>
2022-03-06 10:14:16 +0100

    added short-circuiting to IsGreaterThan

--- Code/Light.Validation.Tests/Checks/IsGreaterThanTests.cs
+++ Code/Light.Validation.Tests/Checks/IsGreaterThanTests.cs
@@ -34,9 +34,10 @@ public static class IsGreaterThanTests
         var dto = new Dto<int> { SomeValue = value };
         var context = new ValidationContext();
 
-        context.Check(dto.SomeValue).IsGreaterThan(comparativeValue);
+        var check = context.Check(dto.SomeValue).IsGreaterThan(comparativeValue);
 
         context.ShouldHaveSingleError("someValue", $"someValue must be greater than {comparativeValue}.");
+        check.ShouldNotBeShortCircuited();
     }
 
     [Theory]
@@ -46,9 +47,10 @@ public static class IsGreaterThanTests
         var dto = new Dto<decimal> { SomeValue = value };
         var context = new ValidationContext();
 
-        context.Check(dto.SomeValue).IsGreaterThan(comparativeValue);
+        var check = context.Check(dto.SomeValue).IsGreaterThan(comparativeValue);
 
         context.ShouldHaveNoErrors();
+        check.ShouldNotBeShortCircuited();
     }
 
     [Theory]
@@ -58,9 +60,38 @@ public static class IsGreaterThanTests
         var dto = new Dto<int> { SomeValue = value };
         var context = new ValidationContext();
 
-        context.Check(dto.SomeValue).IsGreaterThan(comparativeValue, "It must be greater!");
+        var check = context.Check(dto.SomeValue).IsGreaterThan(comparativeValue, "It must be greater!");
 
         context.ShouldHaveSingleError("someValue", "It must be greater!");
+        check.ShouldNotBeShortCircuited();
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidValues))]
+    public static void NoErrorOnShortCircuitedCheck(int value, int comparativeValue)
+    {
+        var dto = new Dto<int> { SomeValue = value };
+        var context = new ValidationContext();
+
+        var check = context.Check(dto.SomeValue)
+                           .ShortCircuit()
+                           .IsGreaterThan(comparativeValue);
+
+        context.ShouldHaveNoErrors();
+        check.ShouldBeShortCircuited();
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidValues))]
+    public static void ShortCircuit(int value, int comparativeValue)
+    {
+        var dto = new Dto<int> { SomeValue = value };
+        var context = new ValidationContext();
+
+        var check = context.Check(dto.SomeValue).IsGreaterThan(comparativeValue, shortCircuitOnError: true);
+
+        context.ShouldHaveErrors();
+        check.ShouldBeShortCircuited();
     }
 
     [Fact]
@@ -69,9 +100,14 @@ public static class IsGreaterThanTests
         var dto = new Dto<double> { SomeValue = 1.337 };
         var context = new ValidationContext();
 
-        context.Check(dto.SomeValue).IsGreaterThan(2.701, (c, o) => $"{c.Key} must not be less than or equal to {o.ToString(CultureInfo.InvariantCulture)}, but you provided {c.Value.ToString(CultureInfo.InvariantCulture)}");
+        var check = context.Check(dto.SomeValue)
+                           .IsGreaterThan(
+                                2.701,
+                                (c, o) => $"{c.Key} must not be less than or equal to {o.ToString(CultureInfo.InvariantCulture)}, but you provided {c.Value.ToString(CultureInfo.InvariantCulture)}"
+                            );
 
         context.ShouldHaveSingleError("someValue", "someValue must not be less than or equal to 2.701, but you provided 1.337");
+        check.ShouldNotBeShortCircuited();
     }
 
     [Fact]
@@ -80,11 +116,38 @@ public static class IsGreaterThanTests
         var dto = new Dto<string> { SomeValue = "Z" };
         var context = new ValidationContext();
 
-        context.Check(dto.SomeValue).IsGreaterThan("z", (_, _) => "I don't care");
+        var check = context.Check(dto.SomeValue).IsGreaterThan("z", (_, _) => "I don't care");
 
         context.ShouldHaveNoErrors();
+        check.ShouldNotBeShortCircuited();
     }
-    
+
+    [Fact]
+    public static void ShortCircuitWithCustomMessageFactory()
+    {
+        var dto = new Dto<string> { SomeValue = "G" };
+        var context = new ValidationContext();
+
+        var check = context.Check(dto.SomeValue).IsGreaterThan("J", (c, _) => $"{c.Key} is too small", true);
+
+        context.ShouldHaveErrors();
+        check.ShouldBeShortCircuited();
+    }
+
+    [Fact]
+    public static void NoErrorOnShortCircuitedCheckWithCustomMessageFactory()
+    {
+        var dto = new Dto<short> { SomeValue = -14 };
+        var context = new ValidationContext();
+
+        var check = context.Check(dto.SomeValue)
+                           .ShortCircuit()
+                           .IsGreaterThan((short) 0, (_, _) => "whatever");
+
+        context.ShouldHaveNoErrors();
+        check.ShouldBeShortCircuited();
+    }
+
     private sealed class Dto<T> where T : IComparable<T>
     {
         public T SomeValue { get; init; } = default!;
--- Code/Light.Validation/Checks/Checks.Comparable.cs
+++ Code/Light.Validation/Checks/Checks.Comparable.cs
@@ -16,20 +16,25 @@ public static partial class Checks
     /// The error message that will be added to the context (optional). If null is provided, the default error
     /// message will be created from the error templates associated to the validation context.
     /// </param>
+    /// <param name="shortCircuitOnError">
+    /// The value indicating whether the check instance is short-circuited when validation fails.
+    /// Short-circuited instances will not perform any more checks.
+    /// </param>
     /// <typeparam name="T">The type of the value to be checked.</typeparam>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="comparativeValue" /> is null.</exception>
-    public static Check<T> IsGreaterThan<T>(this Check<T> check, T comparativeValue, string? message = null)
+    public static Check<T> IsGreaterThan<T>(this Check<T> check,
+                                            T comparativeValue,
+                                            string? message = null,
+                                            bool shortCircuitOnError = false)
         where T : IComparable<T>
     {
         comparativeValue.MustNotBeNullReference();
 
-        if (check.IsValueNull)
+        if (check.IsShortCircuited || check.IsValueNull || check.Value.CompareTo(comparativeValue) > 0)
             return check;
 
-        if (check.Value.CompareTo(comparativeValue) <= 0)
-            check.AddGreaterThanError(comparativeValue, message);
-
-        return check;
+        check.AddGreaterThanError(comparativeValue, message);
+        return check.ShortCircuitIfNecessary(shortCircuitOnError);
     }
 
     /// <summary>
@@ -39,21 +44,27 @@ public static partial class Checks
     /// <param name="check">The structure that encapsulates the value to be checked and the validation context.</param>
     /// <param name="comparativeValue">The other value the actual value is compared against.</param>
     /// <param name="errorMessageFactory">The delegate that is used to create the error message.</param>
+    /// <param name="shortCircuitOnError">
+    /// The value indicating whether the check instance is short-circuited when validation fails.
+    /// Short-circuited instances will not perform any more checks.
+    /// </param>
     /// <typeparam name="T">The type of the value to be checked.</typeparam>
     /// <exception cref="ArgumentNullException">
     /// Thrown when <paramref name="comparativeValue" /> or <paramref name="errorMessageFactory"/> is null.
     /// </exception>
     public static Check<T> IsGreaterThan<T>(this Check<T> check,
                                             T comparativeValue,
-                                            Func<Check<T>, T, string> errorMessageFactory)
+                                            Func<Check<T>, T, string> errorMessageFactory,
+                                            bool shortCircuitOnError = false)
         where T : IComparable<T>
     {
         comparativeValue.MustNotBeNullReference();
 
-        if (!check.IsValueNull && check.Value.CompareTo(comparativeValue) <= 0)
-            check.AddError(errorMessageFactory, comparativeValue);
+        if (check.IsShortCircuited || check.IsValueNull || check.Value.CompareTo(comparativeValue) > 0)
+            return check;
 
-        return check;
+        check.AddError(errorMessageFactory, comparativeValue);
+        return check.ShortCircuitIfNecessary(shortCircuitOnError);
     }
 
     /// <summary>
