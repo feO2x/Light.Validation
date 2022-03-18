58fe9b21d2fcd5ca60b4424c62d626b824f8da18
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-03-18 17:07:38 +0100

    Added Dto for first scenario with flat structure and two parameters

---
+++ Code/Bachelor.Thesis.Benchmarking/FlatTwoParametersDto/FlatTwoParametersDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Bachelor.Thesis.Benchmarking.FlatTwoParametersDto;
+
+public class FlatTwoParametersDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+}
