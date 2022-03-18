03010831683a8b2429b2c3794c45401d92997fb5
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-03-18 17:06:15 +0100

    created new project in Ligth.Validation solution

---
+++ Code/Bachelor.Thesis.Benchmarking/Program.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace Bachelor.Thesis.Benchmarking;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run(typeof(Program).Assembly, CreateDefaultConfig(), args);
+    }
+
+    public static IConfig CreateDefaultConfig() =>
+        DefaultConfig.Instance
+                     .AddJob(Job.Default.WithRuntime(CoreRuntime.Core60))
+                     .AddDiagnoser(MemoryDiagnoser.Default);
+}
