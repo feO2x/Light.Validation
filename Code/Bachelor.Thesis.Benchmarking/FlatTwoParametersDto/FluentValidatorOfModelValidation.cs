62f92870f00a08fea2b4238877f338b046203238
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-03-21 16:20:58 +0100

    Added classes for Ligth and Fluent Validator, that use ModelValidationDto instead of completely flat Dto

---
+++ Code/Bachelor.Thesis.Benchmarking/FlatTwoParametersDto/FluentValidatorOfModelValidation.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace Bachelor.Thesis.Benchmarking.FlatTwoParametersDto;
+
+public class FluentValidatorOfModelValidation : AbstractValidator<ModelValidationDto>
+{
+    public FluentValidatorOfModelValidation()
+    {
+        RuleFor(dto => dto.Id).GreaterThan(0);
+        RuleFor(dto => dto.Name).NotEmpty();
+    }
+
+    protected override bool PreValidate(ValidationContext<ModelValidationDto> context, ValidationResult result)
+    {
+        // ReSharper disable once ConstantConditionalAccessQualifier -- value can be null
+        context.InstanceToValidate.Name = context.InstanceToValidate.Name?.Trim() ?? string.Empty;
+        return true;
+    }
+}
---
+++ Code/Bachelor.Thesis.Benchmarking/FlatTwoParametersDto/LightValidatorOfModelValidation.cs
@@ -0,0 +1,16 @@
+﻿using Light.Validation;
+using Light.Validation.Checks;
+
+namespace Bachelor.Thesis.Benchmarking.FlatTwoParametersDto;
+
+public class LightValidatorOfModelValidation : Validator<ModelValidationDto>
+{
+    protected override void PerformValidation(ValidationContext context, ModelValidationDto value)
+    {
+        context.Check(value.Id).IsGreaterThan(0);
+        value.Name = context.Check(value.Name)
+                            .Normalize()
+                            .IsNotNullOrWhiteSpace()
+                            .Value;
+    }
+}
