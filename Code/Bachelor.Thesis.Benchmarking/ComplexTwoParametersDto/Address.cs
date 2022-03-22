6a8885e852a7a25c58860e76828b9b53d3705c44
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-03-22 15:51:41 +0100

    Added address class as object for further benchmarking

---
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/Address.cs
@@ -0,0 +1,23 @@
+﻿namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
+
+public class Address
+{
+    public static Address Default = new ()
+    {
+        Country = "Germany",
+        City = "Regensburg",
+        Region = "Bavaria",
+        PostalCode = 93053,
+        Street = "Seybothstraße 2"
+    };
+
+    public string? City { get; set; }
+
+    public string? Country { get; set; }
+
+    public long PostalCode { get; set; }
+
+    public string? Region { get; set; }
+
+    public string? Street { get; set; }
+}
