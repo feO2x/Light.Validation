18849d39489a84691a077de159d249a4bdaa4977
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-03-22 15:52:04 +0100

    added a more complex Dto with two parameters of not primitive data types

---
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/ComplexTwoParametersDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
+
+public class ComplexTwoParametersDto
+{
+    public List<string>? ListOfStrings { get; set; }
+
+    public Address? Address { get; set; }
+}
