a40b987257ad4e375d61727c136fd0f3d3fef242
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-03-23 16:16:32 +0100

    Added ModelValidation Dtos

---
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/ModelValidationAddressDto.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
+
+public class ModelValidationAddressDto
+{
+    public static Address ValidAddress = new()
+    {
+        Country = "Germany",
+        City = "Regensburg",
+        Region = "Bavaria",
+        PostalCode = 93053,
+        Street = "Seybothstraße 2"
+    };
+
+    [Required]
+    [MinLength(1)]
+    public string City { get; set; } = String.Empty;
+
+    [Required]
+    [MinLength(1)]
+    public string Country { get; set; } = String.Empty;
+
+    [Required]
+    [Range(10000, 99999)]
+    public int PostalCode { get; set; }
+
+    [Required]
+    [MinLength(1)]
+    public string Region { get; set; } = String.Empty;
+
+    [Required]
+    [MinLength(1)]
+    public string Street { get; set; } = String.Empty;
+}
---
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/ModelValidationDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
+
+public class ModelValidationDto
+{
+    [Required]
+    [MinLength(1)]
+    public List<string> ListOfStrings { get; set; } = new();
+
+    [Required]
+    public ModelValidationAddressDto Address { get; set; } = new();
+}
