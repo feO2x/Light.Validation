c6cd2921bc2c7e084e111f8ea484c09771fa9c64
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-03-23 16:15:41 +0100

    Added FluentValidator for ComplexTwoParametersDto

---
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/FluentValidator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
+
+public class FluentValidator : AbstractValidator<ComplexTwoParametersDto>
+{
+    public FluentValidator()
+    {
+        RuleFor(dto => dto.ListOfStrings.Count).GreaterThanOrEqualTo(1);
+
+        RuleFor(dto => dto.Address.Country).NotEmpty();
+        RuleFor(dto => dto.Address.Region).NotEmpty();
+        RuleFor(dto => dto.Address.City).NotEmpty();
+        RuleFor(dto => dto.Address.Street).NotEmpty();
+        RuleFor(dto => dto.Address.PostalCode).InclusiveBetween(10000, 99999);
+    }
+
+    [SuppressMessage("ReSharper", "ConstantConditionalAccessQualifier")]
+    protected override bool PreValidate(ValidationContext<ComplexTwoParametersDto> context, ValidationResult result)
+    {
+        context.InstanceToValidate.Address.Country = context.InstanceToValidate.Address.Country?.Trim() ?? String.Empty;
+        context.InstanceToValidate.Address.Region = context.InstanceToValidate.Address.Region?.Trim() ?? String.Empty;
+        context.InstanceToValidate.Address.City = context.InstanceToValidate.Address.City?.Trim() ?? String.Empty;
+        context.InstanceToValidate.Address.Street = context.InstanceToValidate.Address.Street?.Trim() ?? String.Empty;
+
+        return true;
+    }
+}
