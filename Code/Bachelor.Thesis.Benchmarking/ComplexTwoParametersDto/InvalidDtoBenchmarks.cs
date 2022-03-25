f448602dcc883cac8f1648da701452b345f2590d
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-03-25 15:00:08 +0100

    Added Benchmarks for a invalid Dto

---
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/InvalidDtoBenchmarks.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BenchmarkDotNet.Attributes;
+
+namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
+
+public class InvalidDtoBenchmarks
+{
+    public ComplexTwoParametersDto Dto = new() { Names = new() { "John" }, Address = Address.InvalidAddress };
+
+    public FluentValidator FluentValidator = new();
+    public LightValidator LightValidator = new();
+
+    public ModelValidationDto ModelValidationDto = new() { Names = new() { "John" }, Address = ModelValidationAddress.InvalidAddress };
+
+    [Benchmark(Baseline = true)]
+    public object? CheckViaLightValidator()
+    {
+        LightValidator.CheckForErrors(Dto, out var errors);
+        return errors;
+    }
+
+    [Benchmark]
+    public object? CheckViaFluentValidator() =>
+        FluentValidator.Validate(Dto);
+
+    [Benchmark]
+    public object CheckViaModelValidation()
+    {
+        var errors = new List<ValidationResult>();
+        Validator.TryValidateObject(ModelValidationDto, new ValidationContext(ModelValidationDto), errors, true);
+        return errors;
+    }
+}
