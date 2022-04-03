59077dd95e08584b998eb91d4cc9043f2cedb4ce
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-03 15:12:46 +0200

    Implemented changes to LightValidators because of new version

--- Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/LightValidator.cs
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/LightValidator.cs
@@ -1,36 +1,33 @@
 ﻿using Light.Validation;
 using Light.Validation.Checks;
-using Light.Validation.Tools;
 using Range = Light.Validation.Tools.Range;
 
 namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
 
 public class LightValidator : Validator<ComplexTwoParametersDto>
 {
-    protected override void PerformValidation(ValidationContext context, ComplexTwoParametersDto value)
+    protected override ComplexTwoParametersDto PerformValidation(ValidationContext context, ComplexTwoParametersDto value)
     {
-        context.Check(value.Names!.Count).IsGreaterThanOrEqualTo(2);
+        context.Check(value.Names.Count).IsGreaterThanOrEqualTo(2);
 
         value.Address.Country = context.Check(value.Address.Country)
-                                       .Normalize()
-                                       .IsNotNullOrWhiteSpace()
-                                       .Value;
+                                         .Normalize()
+                                         .IsNotNullOrWhiteSpace();
 
         value.Address.Region = context.Check(value.Address.Region)
-                                       .Normalize()
-                                       .IsNotNullOrWhiteSpace()
-                                       .Value;
+                                        .Normalize()
+                                        .IsNotNullOrWhiteSpace();
 
         value.Address.City = context.Check(value.Address.City)
-                                       .Normalize()
-                                       .IsNotNullOrWhiteSpace()
-                                       .Value;
+                                      .Normalize()
+                                      .IsNotNullOrWhiteSpace();
 
         value.Address.Street = context.Check(value.Address.Street)
-                                       .Normalize()
-                                       .IsNotNullOrWhiteSpace()
-                                       .Value;
+                                        .Normalize()
+                                        .IsNotNullOrWhiteSpace();
 
-        context.Check(value.Address.PostalCode).IsIn(Range.FromInclusive(10000).ToInclusive(99999));
+        value.Address.PostalCode = context.Check(value.Address.PostalCode).IsIn(Range.FromInclusive(10000).ToInclusive(99999));
+
+        return value;
     }
 }
--- Code/Bachelor.Thesis.Benchmarking/FlatTwoParametersDto/LightValidator.cs
+++ Code/Bachelor.Thesis.Benchmarking/FlatTwoParametersDto/LightValidator.cs
@@ -5,12 +5,14 @@ namespace Bachelor.Thesis.Benchmarking.FlatTwoParametersDto;
 
 public class LightValidator : Validator<FlatTwoParametersDto>
 {
-    protected override void PerformValidation(ValidationContext context, FlatTwoParametersDto value)
+    protected override FlatTwoParametersDto PerformValidation(ValidationContext context, FlatTwoParametersDto value)
     {
-        context.Check(value.Id).IsGreaterThan(0);
+        value.Id = context.Check(value.Id).IsGreaterThan(0);
+
         value.Name = context.Check(value.Name)
-                            .Normalize()
-                            .IsNotNullOrWhiteSpace()
-                            .Value;
+                              .Normalize()
+                              .IsNotNullOrWhiteSpace();
+
+        return value;
     }
 }
