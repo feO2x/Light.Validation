ce858364fa414d918b793f973740d37aeced1c8b
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-03 15:07:17 +0200

    Added Dto with eight primitive parameters

---
+++ Code/Bachelor.Thesis.Benchmarking/FlatEightParametersDto/FlatEightParametersDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Bachelor.Thesis.Benchmarking.FlatEightParametersDto;
+
+public class FlatEightParametersDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public bool Active { get; set; } = false;
+
+    public string Role { get; set; } = string.Empty;
+}
