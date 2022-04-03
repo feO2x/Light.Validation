1359e96175e60a7742134df92d641bd2ca0efb87
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-03 16:43:22 +0200

    Updated User with ModelValidation Rules, last commit with this statement was an error

--- Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/User.cs
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/User.cs
@@ -1,16 +1,52 @@
-﻿namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
 
 public class User
 {
-    public string UserName { get; set; }
+    public static User ValidUser = new ()
+    {
+        UserName = "JohnDoe1337",
+        Password = "P4S$W0rD123",
+        ForeName = "John",
+        LastName = "Doe",
+        Active = true,
+        Age = 42
+    };
 
-    public string Password { get; set; }
+    public static User InvalidUser = new ()
+    {
+        UserName = "JohnDoe",
+        Password = "P4S$W0r",
+        ForeName = "J",
+        LastName = "D",
+        Age = 16
+    };
 
-    public string ForeName { get; set; }
+    [Required]
+    [MinLength(8)]
+    [MaxLength(30)]
+    public string UserName { get; set; } = string.Empty;
 
-    public string LastName { get; set; }
+    // password must contain minimum of eight characters with at least one letter and one number
+    [Required]
+    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")]
+    public string Password { get; set; } = string.Empty;
 
-    public bool Active { get; set; }
+    [Required]
+    [MinLength(2)]
+    [MaxLength(80)]
+    public string ForeName { get; set; } = string.Empty;
 
+    [Required]
+    [MinLength(2)]
+    [MaxLength(80)]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    public bool Active { get; set; } = true;
+
+    [Required]
+    [Range(18, 130)]
     public int Age { get; set; }
 }
