4759912a347aa294d672270d076aef88aa6e56ae
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-03 16:42:06 +0200

    Added ModelValidation annotations to Dto, added static default values

--- Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/ComplexTwoParametersDto.cs
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/ComplexTwoParametersDto.cs
@@ -5,8 +5,7 @@ namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
 public class ComplexTwoParametersDto
 {
     [Required]
-    [MinLength(2)]
-    public List<string> Names { get; set; } = new();
+    public User User { get; set; } = new ();
 
     [Required]
     public Address Address { get; set; } = new();
