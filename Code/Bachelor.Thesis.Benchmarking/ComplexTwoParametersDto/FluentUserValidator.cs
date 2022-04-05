893688ec63490bc28c172c583264b48cb2820354
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-05 13:44:42 +0200

    Added FluentUserValidator

---
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/FluentUserValidator.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
+
+public class FluentUserValidator : AbstractValidator<User>
+{
+    public FluentUserValidator()
+    {
+        RuleFor(user => user.UserName).NotEmpty().MinimumLength(8).MaximumLength(30);
+        RuleFor(user => user.Password).NotEmpty().Matches(new Regex("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"));
+        RuleFor(user => user.Name).NotEmpty().MinimumLength(2).MaximumLength(100);
+        RuleFor(user => user.Email).NotEmpty().EmailAddress();
+        RuleFor(user => user.Age).InclusiveBetween(18, 130);
+    }
+
+    protected override bool PreValidate(ValidationContext<User> context, ValidationResult result)
+    {
+        context.InstanceToValidate.UserName = context.InstanceToValidate.UserName.Trim();
+        context.InstanceToValidate.Password = context.InstanceToValidate.Password.Trim();
+        context.InstanceToValidate.Name = context.InstanceToValidate.Name.Trim();
+        context.InstanceToValidate.Email = context.InstanceToValidate.Email.Trim();
+
+        return true;
+    }
+}
