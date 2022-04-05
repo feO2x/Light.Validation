6235af2df587c48b70c20221de61d24ec15440ff
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-05 13:58:27 +0200

    Added maxValue validation in FluentValidation

--- Code/Bachelor.Thesis.Benchmarking/FlatTwoParametersDto/Validators/FluentValidator.cs
+++ Code/Bachelor.Thesis.Benchmarking/FlatTwoParametersDto/Validators/FluentValidator.cs
@@ -7,14 +7,13 @@ public class FluentValidator : AbstractValidator<FlatTwoParametersDto>
 {
     public FluentValidator()
     {
-        RuleFor(dto => dto.Id).GreaterThan(0);
+        RuleFor(dto => dto.Id).GreaterThan(0).LessThan(int.MaxValue);
         RuleFor(dto => dto.Name).NotEmpty().MaximumLength(80);
     }
 
     protected override bool PreValidate(ValidationContext<FlatTwoParametersDto> context, ValidationResult result)
     {
-        // ReSharper disable once ConstantConditionalAccessQualifier -- value can be null
-        context.InstanceToValidate.Name = context.InstanceToValidate.Name?.Trim() ?? string.Empty;
+        context.InstanceToValidate.Name = context.InstanceToValidate.Name.Trim();
         return true;
     }
 }
