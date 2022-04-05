55ee5a4c8828a5afbbb36a85906a060ecce6e0ad
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-05 13:25:37 +0200

    Implemented Address and UserValidator in base LightDtoValidator

--- Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/LightDtoValidator.cs
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/LightDtoValidator.cs
@@ -1,39 +1,20 @@
-﻿using System.Text.RegularExpressions;
-using Light.Validation;
+﻿using Light.Validation;
 using Light.Validation.Checks;
-using Range = Light.Validation.Tools.Range;
 
 namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
 
 public class LightDtoValidator : Validator<ComplexTwoParametersDto>
 {
-    protected override ComplexTwoParametersDto PerformValidation(ValidationContext context, ComplexTwoParametersDto value)
+    private LightAddressValidator AddressValidator { get; } = new ();
+
+    private LightUserValidator UserValidator { get; } = new ();
+
+    protected override ComplexTwoParametersDto PerformValidation(ValidationContext context, ComplexTwoParametersDto dto)
     {
-        // TODO: check User and Address in own validator classes
-        value.User.UserName = context.Check(value.User.UserName)
-                                       .IsLongerThan(8)
-                                       .IsShorterThan(30);
-        value.User.Password = context.Check(value.User.Password)
-                                       .IsMatching(new Regex("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"));
-        value.User.Name = context.Check(value.User.Name)
-                                   .IsLongerThan(2)
-                                   .IsShorterThan(100);
-        value.User.Email = context.Check(value.User.Email)
-                                    .IsEmail();
-        value.User.Age = context.Check(value.User.Age)
-                                  .IsIn(Range.FromInclusive(18).ToInclusive(130));
+        dto.User = context.Check(dto.User).ValidateWith(UserValidator);
 
-        value.Address.Country = context.Check(value.Address.Country)
-                                         .IsNotNullOrWhiteSpace();
-        value.Address.Region = context.Check(value.Address.Region)
-                                        .IsNotNullOrWhiteSpace();
-        value.Address.City = context.Check(value.Address.City)
-                                      .IsNotNullOrWhiteSpace();
-        value.Address.Street = context.Check(value.Address.Street)
-                                        .IsNotNullOrWhiteSpace();
-        value.Address.PostalCode = context.Check(value.Address.PostalCode)
-                                            .IsIn(Range.FromInclusive(10000).ToInclusive(99999));
+        dto.Address = context.Check(dto.Address).ValidateWith(AddressValidator);
 
-        return value;
+        return dto;
     }
 }
