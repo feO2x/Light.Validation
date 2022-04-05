7acc50c014d9360778eb977aaf64397ae3b0d449
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-05 13:10:03 +0200

    Added LightAddressValidator to slim down LightDtoValidator

---
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/LightAddressValidator.cs
@@ -0,0 +1,32 @@
+﻿using Light.Validation;
+using Light.Validation.Checks;
+using Range = Light.Validation.Tools.Range;
+
+namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
+
+public class LightAddressValidator : Validator<Address>
+{
+    protected override Address PerformValidation(ValidationContext context, Address address)
+    {
+        address.City = context.Check(address.City)
+                              .IsLongerThan(1)
+                              .IsShorterThan(40);
+
+        address.Country = context.Check(address.Country)
+                                 .IsLongerThan(1)
+                                 .IsShorterThan(40);
+
+        address.PostalCode = context.Check(address.PostalCode)
+                                    .IsIn(Range.FromInclusive(10000).ToInclusive(99999));
+
+        address.Region = context.Check(address.Region)
+                                .IsLongerThan(1)
+                                .IsShorterThan(30);
+
+        address.Street = context.Check(address.Street)
+                                .IsLongerThan(1)
+                                .IsShorterThan(50);
+
+        return address;
+    }
+}
