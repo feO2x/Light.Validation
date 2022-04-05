3e84eb6a1b19c43efd5d6113d8b39465eea4fafb
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-05 13:19:11 +0200

    Added LightUserValidator to slim down LightDtoValidator

---
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParametersDto/LightUserValidator.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+using Light.Validation;
+using Light.Validation.Checks;
+using Range = Light.Validation.Tools.Range;
+
+namespace Bachelor.Thesis.Benchmarking.ComplexTwoParametersDto;
+
+public class LightUserValidator : Validator<User>
+{
+    protected override User PerformValidation(ValidationContext context, User user)
+    {
+        user.UserName = context.Check(user.UserName)
+                               .IsLongerThan(8)
+                               .IsShorterThan(30);
+
+        user.Password = context.Check(user.Password)
+                               .IsMatching(new Regex(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"));
+
+        user.Name = context.Check(user.Name)
+                           .IsLongerThan(2)
+                           .IsShorterThan(100);
+
+        user.Email = context.Check(user.Email)
+                            .IsEmail();
+
+        user.Age = context.Check(user.Age)
+                          .IsIn(Range.FromInclusive(18).ToInclusive(130));
+
+        return user;
+    }
+}
