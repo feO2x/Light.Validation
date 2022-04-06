576d132c249165967dd06af28405c114f1daab54
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-06 17:21:12 +0200

    Added benchmarks for invalid Dtos

---
+++ Code/Bachelor.Thesis.Benchmarking/FlatEightParametersDto/InvalidDtoBenchmarks.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bachelor.Thesis.Benchmarking.FlatEightParametersDto.Validators;
+using BenchmarkDotNet.Attributes;
+
+namespace Bachelor.Thesis.Benchmarking.FlatEightParametersDto;
+
+public class InvalidDtoBenchmarks
+{
+    public Employee Employee = Employee.InvalidEmployee;
+
+    public FluentValidator FluentValidator = new();
+
+    public LightValidator LightValidator = new();
+
+    [Benchmark(Baseline = true)]
+    public object? CheckViaLightValidator()
+    {
+        LightValidator.CheckForErrors(Employee, out var errors);
+        return errors;
+    }
+
+    [Benchmark]
+    public object? CheckViaFluentValidator() =>
+        FluentValidator.Validate(Employee);
+
+    [Benchmark]
+    public object CheckViaModelValidation()
+    {
+        var errors = new List<ValidationResult>();
+        Validator.TryValidateObject(Employee, new ValidationContext(Employee), errors, true);
+        return errors;
+    }
+}
