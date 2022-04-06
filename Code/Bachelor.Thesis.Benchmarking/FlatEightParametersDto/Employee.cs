b9b5ba47287025490cf8b6b48c8ab1d701caf5a3
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-06 15:19:49 +0200

    Changed name of Dto to employees, renamed case, added ModelValidation annotations

---
+++ Code/Bachelor.Thesis.Benchmarking/FlatEightParametersDto/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bachelor.Thesis.Benchmarking.FlatTenParametersDto;
+
+public class Employee
+{
+    [Required]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MinLength(2)]
+    [MaxLength(80)]
+    public string Name { get; set; } = string.Empty;
+    
+    [Required]
+    [Range(100, 999)]
+    public short Department { get; set; }
+
+    [Required]
+    [Range(20, 48)]
+    public int WeeklyWorkingHours { get; set; }
+
+    [Required]
+    [Phone]
+    public long PhoneNumber { get; set; }
+
+    [Required]
+    [Range(float.MinValue, float.MaxValue)]
+    public float OvertimeWorked { get; set; }
+
+    [Required]
+    [Range(12.0, 999.9)]
+    public decimal HourlySalary { get; set; }
+}
