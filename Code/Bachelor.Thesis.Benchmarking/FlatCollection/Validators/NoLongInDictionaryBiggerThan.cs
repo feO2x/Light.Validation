9f5938efaec42c431ff0e8119b0411e7b2831bd2
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-09 17:37:06 +0200

    Added custom model validation for long in dictionary

---
+++ Code/Bachelor.Thesis.Benchmarking/FlatCollection/Validators/NoLongInDictionaryBiggerThan.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bachelor.Thesis.Benchmarking.FlatCollection.Validators;
+
+public class NoLongInDictionaryBiggerThan : ValidationAttribute
+{
+    private readonly int _maxValue;
+
+    public NoLongInDictionaryBiggerThan(int maxValue) =>
+        _maxValue = maxValue;
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var dictionary = value as IDictionary<long, bool>;
+        if (dictionary == null)
+            return ValidationResult.Success;
+
+        var invalid = dictionary.Where(l => l.Key > _maxValue).ToArray();
+        if (invalid.Length > 0)
+        {
+            return new ValidationResult("The following keys exceed the value: " + string.Join(", ", invalid));
+        }
+
+        return ValidationResult.Success;
+    }
+}
