4539aff4a1ba789b8b878427c64e0d5e91ea34e8
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-10 12:30:11 +0200

    Added fluentValidation for collection case

---
+++ Code/Bachelor.Thesis.Benchmarking/FlatCollection/Validators/FluentDtoValidation.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Bachelor.Thesis.Benchmarking.FlatCollection.Validators;
+
+public class FluentDtoValidation : AbstractValidator<FlatCollection>
+{
+    public FluentDtoValidation()
+    {
+        RuleFor(dto => dto.Names).NotEmpty();
+        RuleFor(dto => dto.Names.Count).InclusiveBetween(1, 10);
+        RuleForEach(dto => dto.Names).MaximumLength(20);
+
+        RuleFor(dto => dto.Availability).NotEmpty();
+        RuleForEach(dto => dto.Availability.Keys).LessThanOrEqualTo(10000);
+    }
+}
