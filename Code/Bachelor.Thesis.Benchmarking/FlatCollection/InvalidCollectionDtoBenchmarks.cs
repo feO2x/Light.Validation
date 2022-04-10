3059191c573ac52abcda9fa28ca0a83dc493396c
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-10 12:49:21 +0200

    Added benchmarks for invalid collections

---
+++ Code/Bachelor.Thesis.Benchmarking/FlatCollection/InvalidCollectionDtoBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bachelor.Thesis.Benchmarking.FlatCollection.Validators;
+using BenchmarkDotNet.Attributes;
+
+namespace Bachelor.Thesis.Benchmarking.FlatCollection;
+
+public class InvalidCollectionDtoBenchmarks
+{
+    public FlatCollection Dto = FlatCollection.InvalidDto;
+
+    public FluentDtoValidator FluentValidator = new();
+    public LightDtoValidator LightValidator = new();
+
+    [Benchmark(Baseline = true)]
+    public object? CheckViaLightValidator()
+    {
+        LightValidator.CheckForErrors(Dto, out var errors);
+        return errors;
+    }
+
+    [Benchmark]
+    public object? CheckViaFluentValidator() =>
+        FluentValidator.Validate(Dto);
+
+    [Benchmark]
+    public object CheckViaModelValidation()
+    {
+        var errors = new List<ValidationResult>();
+        Validator.TryValidateObject(Dto, new ValidationContext(Dto), errors, true);
+        return errors;
+    }
+}
