02b75c23eac748db94f0c591609641a407185280
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-10 12:30:20 +0200

    added LightValidation for collection case

---
+++ Code/Bachelor.Thesis.Benchmarking/FlatCollection/Validators/LightDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using Light.Validation;
+using Light.Validation.Checks;
+using Range = Light.Validation.Tools.Range;
+
+namespace Bachelor.Thesis.Benchmarking.FlatCollection.Validators;
+
+public class LightDtoValidator : Validator<FlatCollection>
+{
+    protected override FlatCollection PerformValidation(ValidationContext context, FlatCollection collectionDto)
+    {
+        collectionDto.Names = context.Check(collectionDto.Names).IsNotNull();
+        context.Check(collectionDto.Names.Count).IsIn(Range.FromInclusive(1).ToInclusive(10));
+
+
+        return collectionDto;
+    }
+}
