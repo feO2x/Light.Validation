7875b166aee2529c8423bde04b5a0bddb864606c
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-10 12:48:57 +0200

    Added benchmarks for valid collections

--- Code/Bachelor.Thesis.Benchmarking/FlatCollection/ValidCollectionDtoBenchmarks.cs
+++ Code/Bachelor.Thesis.Benchmarking/FlatCollection/ValidCollectionDtoBenchmarks.cs
@@ -1,6 +1,32 @@
-﻿namespace Bachelor.Thesis.Benchmarking.FlatCollection;
+﻿using System.ComponentModel.DataAnnotations;
+using Bachelor.Thesis.Benchmarking.FlatCollection.Validators;
+using BenchmarkDotNet.Attributes;
+
+namespace Bachelor.Thesis.Benchmarking.FlatCollection;
 
 public class ValidCollectionDtoBenchmarks
 {
-    public
+    public FlatCollection Dto = FlatCollection.ValidDto;
+
+    public FluentDtoValidator FluentValidator = new ();
+    public LightDtoValidator LightValidator = new ();
+
+    [Benchmark(Baseline = true)]
+    public object? CheckViaLightValidator()
+    {
+        LightValidator.CheckForErrors(Dto, out var errors);
+        return errors;
+    }
+
+    [Benchmark]
+    public object? CheckViaFluentValidator() =>
+        FluentValidator.Validate(Dto);
+
+    [Benchmark]
+    public object CheckViaModelValidation()
+    {
+        var errors = new List<ValidationResult>();
+        Validator.TryValidateObject(Dto, new ValidationContext(Dto), errors, true);
+        return errors;
+    }
 }
