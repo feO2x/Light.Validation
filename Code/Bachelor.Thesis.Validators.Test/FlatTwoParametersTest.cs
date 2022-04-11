7a4cf519d03862e8c53dbbc4c4ab06b4d8cf1c17
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-11 14:25:22 +0200

    Added test for FlatTwoParameter validators

---
+++ Code/Bachelor.Thesis.Validators.Test/FlatTwoParametersTest.cs
@@ -0,0 +1,65 @@
+using System.ComponentModel.DataAnnotations;
+using Bachelor.Thesis.Benchmarking.FlatTwoParameters;
+using Bachelor.Thesis.Benchmarking.FlatTwoParameters.Validators;
+using Light.GuardClauses;
+using Xunit;
+
+namespace Bachelor.Thesis.Validators.Test;
+
+public class FlatTwoParametersTest
+{
+    private readonly UserDto _validUser = UserDto.ValidDto;
+    private readonly UserDto _invalidUser = UserDto.InvalidDto;
+
+    [Fact]
+    public void FluentValidatorValidDtoTest()
+    {
+        var fluentValidator = new FluentValidator();
+        var result = fluentValidator.Validate(_validUser);
+
+        result.IsValid.MustBe(true);
+    }
+
+    [Fact]
+    public void LightValidatorValidDtoTest()
+    {
+        var lightValidator = new LightValidator();
+        var result = lightValidator.Validate(_validUser);
+
+        result.IsValid.MustBe(true);
+    }
+
+    [Fact]
+    public void ModelValidatorValidDtoTest()
+    {
+        var result = Validator.TryValidateObject(_validUser, new ValidationContext(_validUser), null, true);
+
+        result.MustBe(true);
+    }
+
+    [Fact]
+    public void FluentValidatorInvalidDtoTest()
+    {
+        var fluentValidator = new FluentValidator();
+        var result = fluentValidator.Validate(_invalidUser);
+
+        result.IsValid.MustBe(false);
+    }
+
+    [Fact]
+    public void LightValidatorInvalidDtoTest()
+    {
+        var lightValidator = new LightValidator();
+        var result = lightValidator.Validate(_invalidUser);
+
+        result.IsValid.MustBe(false);
+    }
+
+    [Fact]
+    public void ModelValidatorInvalidDtoTest()
+    {
+        var result = Validator.TryValidateObject(_invalidUser, new ValidationContext(_invalidUser), null, true);
+
+        result.MustBe(false);
+    }
+}
