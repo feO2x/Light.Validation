c3f70a7316b9fdda2dcd8d43712e06c88814c09b
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-12 11:41:04 +0200

    Added tests for FlatEightParameters validators

---
+++ Code/Bachelor.Thesis.Tests/Validators/FlatEightParametersValidatorTest.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bachelor.Thesis.Benchmarking.FlatEightParameters;
+using Bachelor.Thesis.Benchmarking.FlatEightParameters.Validators;
+using Light.GuardClauses;
+using Xunit;
+
+namespace Bachelor.Thesis.Tests.Validators;
+
+public class FlatEightParametersValidatorTest
+{
+    private readonly EmployeeDto _validEmployee = EmployeeDto.ValidEmployeeDto;
+    private readonly EmployeeDto _invalidEmployee = EmployeeDto.InvalidEmployeeDto;
+
+    [Fact]
+    public void FluentValidatorValidDtoTest()
+    {
+        var fluentValidator = new FluentValidator();
+        var result = fluentValidator.Validate(_validEmployee);
+
+        result.IsValid.MustBe(true);
+    }
+
+    [Fact]
+    public void LightValidatorValidDtoTest()
+    {
+        var lightValidator = new LightValidator();
+        var result = lightValidator.Validate(_validEmployee);
+
+        result.IsValid.MustBe(true);
+    }
+
+    [Fact]
+    public void ModelValidatorValidDtoTest()
+    {
+        var result = Validator.TryValidateObject(_validEmployee, new ValidationContext(_validEmployee), null, true);
+
+        result.MustBe(true);
+    }
+
+    [Fact]
+    public void FluentValidatorInvalidDtoTest()
+    {
+        var fluentValidator = new FluentValidator();
+        var result = fluentValidator.Validate(_invalidEmployee);
+
+        result.IsValid.MustBe(false);
+    }
+
+    [Fact]
+    public void LightValidatorInvalidDtoTest()
+    {
+        var lightValidator = new LightValidator();
+        var result = lightValidator.Validate(_invalidEmployee);
+
+        result.IsValid.MustBe(false);
+    }
+
+    [Fact]
+    public void ModelValidatorInvalidDtoTest()
+    {
+        var result = Validator.TryValidateObject(_invalidEmployee, new ValidationContext(_invalidEmployee), null, true);
+
+        result.MustBe(false);
+    }
+}
