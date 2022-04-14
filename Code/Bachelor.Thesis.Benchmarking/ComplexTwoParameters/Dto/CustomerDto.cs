d6fdf7e6a89fedf9a68947c4401d7be82fd5b581
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-14 15:47:53 +0200

    Introduced static default values to CustomerDto

--- Code/Bachelor.Thesis.Benchmarking/ComplexTwoParameters/Dto/CustomerDto.cs
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParameters/Dto/CustomerDto.cs
@@ -4,6 +4,17 @@ namespace Bachelor.Thesis.Benchmarking.ComplexTwoParameters.Dto;
 
 public class CustomerDto
 {
+    public static CustomerDto ValidCustomerDto = new()
+    {
+        User = User.ValidUser, 
+        Address = Address.ValidAddress
+    };
+    public static CustomerDto InvalidCustomerDto = new()
+    {
+        User = User.InvalidUser, 
+        Address = Address.InvalidAddress
+    };
+
     [Required]
     public User User { get; set; } = new ();
 
