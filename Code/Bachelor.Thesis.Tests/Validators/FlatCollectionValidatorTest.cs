d06645b2c35a04148ce5c7a1817928f045e646c5
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-20 15:37:00 +0200

    Added tests for flatCollectionValidators

---
+++ Code/Bachelor.Thesis.Tests/Validators/FlatCollectionValidatorTest.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bachelor.Thesis.Benchmarking.FlatCollection;
+using Bachelor.Thesis.Benchmarking.FlatCollection.Validators;
+using Light.GuardClauses;
+using Xunit;
+
+namespace Bachelor.Thesis.Tests.Validators;
+
+public class FlatCollectionValidatorTest
+{
+    private readonly FlatCollection _validCollection = FlatCollection.ValidDto;
+    private readonly FlatCollection _invalidCollection = FlatCollection.InvalidDto;
+
+    [Fact]
+    public void FluentValidatorValidDtoTest()
+    {
+        var fluentValidator = new FluentDtoValidator();
+        var result = fluentValidator.Validate(_validCollection);
+
+        result.IsValid.MustBe(true);
+    }
+
+    [Fact]
+    public void LightValidatorValidDtoTest()
+    {
+        var lightValidator = new LightDtoValidator();
+        var result = lightValidator.Validate(_validCollection);
+
+        result.IsValid.MustBe(true);
+    }
+
+    [Fact]
+    public void ModelValidatorValidDtoTest()
+    {
+        var result = Validator.TryValidateObject(_validCollection, new ValidationContext(_validCollection), null, true);
+
+        result.MustBe(true);
+    }
+
+    [Fact]
+    public void FluentValidatorInvalidDtoTest()
+    {
+        var fluentValidator = new FluentDtoValidator();
+        var result = fluentValidator.Validate(_invalidCollection);
+
+        result.IsValid.MustBe(false);
+    }
+
+    [Fact]
+    public void LightValidatorInvalidDtoTest()
+    {
+        var lightValidator = new LightDtoValidator();
+        var result = lightValidator.Validate(_invalidCollection);
+
+        result.IsValid.MustBe(false);
+    }
+
+    [Fact]
+    public void ModelValidatorInvalidDtoTest()
+    {
+        var result = Validator.TryValidateObject(_invalidCollection, new ValidationContext(_invalidCollection), null, true);
+
+        result.MustBe(false);
+    }
+}
