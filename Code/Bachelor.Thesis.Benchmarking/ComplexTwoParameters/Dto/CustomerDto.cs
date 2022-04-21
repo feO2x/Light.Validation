5b21a942a78c9e72923332cbb59528b692bba5ac
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-21 15:01:05 +0200

    Removed AspNetCore dataAnnotations reference, fixed bug that nested objects won't get validated

--- Code/Bachelor.Thesis.Benchmarking/ComplexTwoParameters/Dto/CustomerDto.cs
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParameters/Dto/CustomerDto.cs
@@ -16,10 +16,8 @@ public class CustomerDto
     };
 
     [Required]
-    [ValidateComplexType] // doesn't work as of right now...
     public User User { get; set; } = new ();
 
     [Required]
-    [ValidateComplexType]
     public Address Address { get; set; } = new ();
 }
