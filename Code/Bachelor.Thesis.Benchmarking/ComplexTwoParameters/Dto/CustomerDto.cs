c7f67894f033d791cd5ea465672182ffadb53d33
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-22 17:22:39 +0200

    Implemented IValidateObject interface to validate nested objects during initial validation

--- Code/Bachelor.Thesis.Benchmarking/ComplexTwoParameters/Dto/CustomerDto.cs
+++ Code/Bachelor.Thesis.Benchmarking/ComplexTwoParameters/Dto/CustomerDto.cs
@@ -2,22 +2,45 @@
 
 namespace Bachelor.Thesis.Benchmarking.ComplexTwoParameters.Dto;
 
-public class CustomerDto
+public class CustomerDto : IValidatableObject
 {
     public static CustomerDto ValidCustomerDto = new()
     {
         User = User.ValidUser, 
         Address = Address.ValidAddress
     };
+
     public static CustomerDto InvalidCustomerDto = new()
     {
         User = User.InvalidUser, 
         Address = Address.InvalidAddress
     };
 
-    [ValidateObject]
+    [Required]
     public User User { get; set; } = new ();
 
-    [ValidateObject]
+    [Required]
     public Address Address { get; set; } = new ();
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        var resultsUser = new List<ValidationResult>();
+        var resultsAddress = new List<ValidationResult>();
+        var results = new List<ValidationResult>();
+
+        Validator.TryValidateObject(User, new ValidationContext(User), resultsUser);
+        Validator.TryValidateObject(Address, new ValidationContext(Address), resultsAddress);
+
+        foreach (var result in resultsUser)
+        {
+            results.Add(result);
+        }
+
+        foreach (var result in resultsAddress)
+        {
+            results.Add(result);
+        }
+
+        return results;
+    }
 }
