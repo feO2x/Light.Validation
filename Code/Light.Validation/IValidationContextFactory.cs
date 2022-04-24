1d4200df2353843ac1fdad31d48067758ff4d6ad
Kenny Pflug <kenny.pflug@live.de>
2022-04-24 20:32:49 +0200

    added IValidationContextFactory interface

---
+++ Code/Light.Validation/IValidationContextFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace Light.Validation;
+
+/// <summary>
+/// Represents the abstraction of a factory that creates validation contexts.
+/// </summary>
+public interface IValidationContextFactory
+{
+    /// <summary>
+    /// Creates a new validation context.
+    /// </summary>
+    ValidationContext CreateInitialValidationContext();
+
+    /// <summary>
+    /// Creates a new validation context, usually used to validate
+    /// a complex child value of a DTO (e.g. a collection or a complex child DTO).
+    /// </summary>
+    /// <typeparam name="T">The type of the child value.</typeparam>
+    /// <param name="parent">The parent validation context.</param>
+    /// <param name="childValue">The child value that requires a new validation context.</param>
+    ValidationContext CreateChildValidationContext<T>(ValidationContext parent, T childValue);
+}
