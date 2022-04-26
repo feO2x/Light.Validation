b8ccaa5011a81eff0efdc3cb8d2a938f9cdf4bb6
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-26 11:59:12 +0200

    Added ModelValidation attributes to article and OrderDetails

--- Code/Bachelor.Thesis.Benchmarking/CollectionComplex/Dto/Article.cs
+++ Code/Bachelor.Thesis.Benchmarking/CollectionComplex/Dto/Article.cs
@@ -1,12 +1,19 @@
-﻿namespace Bachelor.Thesis.Benchmarking.CollectionComplex.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bachelor.Thesis.Benchmarking.CollectionComplex.Dto;
 
 public class Article
 {
+    [Required]
+    [Range(10000, long.MaxValue)]
     public long Id { get; set; }
 
+    [Required]
     public string Title { get; set; } = string.Empty;
 
+    [Required]
     public decimal Price { get; set; }
 
+    [Required]
     public int Quantity { get; set; }
 }
--- Code/Bachelor.Thesis.Benchmarking/CollectionComplex/Dto/OrderDetails.cs
+++ Code/Bachelor.Thesis.Benchmarking/CollectionComplex/Dto/OrderDetails.cs
@@ -1,14 +1,23 @@
-﻿namespace Bachelor.Thesis.Benchmarking.CollectionComplex.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bachelor.Thesis.Benchmarking.CollectionComplex.Dto;
 
 public class OrderDetails
 {
+    [Required]
+    [Range(10000, long.MaxValue)]
     public long OrderId { get; set; }
 
+    [Required]
+    [Range(10000, long.MaxValue)]
     public long ProductId { get; set; }
 
-    public DateTime Date { get; set; }
+    [Required]
+    public DateOnly Date { get; set; }
 
+    [Required]
     public int Quantity { get; set; }
 
+    [Required]
     public decimal PricePaid { get; set; }
 }
