a66b804d3f69548ebd2b614ac4b2bb8c3340c4a5
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-27 10:53:25 +0200

    Added static values to OrderDetails

--- Code/Bachelor.Thesis.Benchmarking/CollectionComplex/Dto/OrderDetails.cs
+++ Code/Bachelor.Thesis.Benchmarking/CollectionComplex/Dto/OrderDetails.cs
@@ -4,6 +4,24 @@ namespace Bachelor.Thesis.Benchmarking.CollectionComplex.Dto;
 
 public class OrderDetails
 {
+    public static OrderDetails ValidOrderDetails = new ()
+    {
+        OrderId = 10010,
+        ProductId = 20010,
+        Date = DateOnly.Parse("2022-04-27"),
+        Quantity = 20,
+        PricePaid = 100
+    };
+
+    public static OrderDetails InvalidOrderDetails = new()
+    {
+        OrderId = 1001,
+        ProductId = 2001,
+        Date = DateOnly.Parse("2023-04-27"),
+        Quantity = -8,
+        PricePaid = 2000000
+    };
+
     [Required]
     [Range(10000, long.MaxValue)]
     public long OrderId { get; set; }
