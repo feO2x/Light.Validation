53ae0348383c962d13c11540c6936e90c8727934
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-27 10:50:52 +0200

    Added modelValidation attributes to OrderDetails class

--- Code/Bachelor.Thesis.Benchmarking/CollectionComplex/Dto/OrderDetails.cs
+++ Code/Bachelor.Thesis.Benchmarking/CollectionComplex/Dto/OrderDetails.cs
@@ -13,11 +13,14 @@ public class OrderDetails
     public long ProductId { get; set; }
 
     [Required]
+    [Range(typeof(DateOnly), "2020-01-01", "2022-12-31")]
     public DateOnly Date { get; set; }
 
     [Required]
+    [Range(0, 1000)]
     public int Quantity { get; set; }
 
     [Required]
+    [Range(0, 100000)]
     public decimal PricePaid { get; set; }
 }
