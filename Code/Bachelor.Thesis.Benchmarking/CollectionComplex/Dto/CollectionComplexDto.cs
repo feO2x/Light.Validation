c7da562d50a9ad3de907d12f4853ffaaf28f565e
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-29 12:22:27 +0200

    Tried another approach of validating complex objects in lists via model validation (failed)

--- Code/Bachelor.Thesis.Benchmarking/CollectionComplex/Dto/CollectionComplexDto.cs
+++ Code/Bachelor.Thesis.Benchmarking/CollectionComplex/Dto/CollectionComplexDto.cs
@@ -3,7 +3,7 @@ using Bachelor.Thesis.Benchmarking.CollectionComplex.ModelValidation;
 
 namespace Bachelor.Thesis.Benchmarking.CollectionComplex.Dto;
 
-public class CollectionComplexDto
+public class CollectionComplexDto : IValidatableObject
 {
     public static CollectionComplexDto ValidDto = new ()
     {
@@ -40,19 +40,46 @@ public class CollectionComplexDto
             Article.InvalidArticle,
             Article.InvalidArticle,
             Article.InvalidArticle,
-            Article.InvalidArticle,
-            Article.InvalidArticle,
             Article.InvalidArticle
         }
     };
 
     [Required]
     [MinLength(1), MaxLength(10)]
-    [ValidateOrderDetailsInList]
+    // [ValidateOrderDetailsInList]
     public List<OrderDetails> OrderDetailsList { get; set; } = new ();
 
     [Required]
     [MinLength(1), MaxLength(10)]
-    [ValidateArticleInList]
+    // [ValidateArticleInList]
     public List<Article> ArticleList { get; set; } = new ();
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        var resultsOrderDetailsList = new List<ValidationResult>();
+        var resultsArticleList = new List<ValidationResult>();
+        var results = new List<ValidationResult>();
+
+        foreach (var order in OrderDetailsList)
+        {
+            Validator.TryValidateObject(order, new ValidationContext(order), resultsOrderDetailsList);
+        }
+
+        foreach (var article in ArticleList)
+        {
+            Validator.TryValidateObject(article, new ValidationContext(article), resultsArticleList);
+        }
+
+        foreach (var result in resultsOrderDetailsList)
+        {
+            results.Add(result);
+        }
+
+        foreach (var result in resultsArticleList)
+        {
+            results.Add(result);
+        }
+
+        return results;
+    }
 }
