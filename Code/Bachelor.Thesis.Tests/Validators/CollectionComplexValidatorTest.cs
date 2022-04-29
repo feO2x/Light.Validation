85ee3663732f09f8e4609dd73a83255d4e145a3f
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-29 16:06:04 +0200

    Added test output to better review the results coming out of validators

--- Code/Bachelor.Thesis.Tests/Validators/CollectionComplexValidatorTest.cs
+++ Code/Bachelor.Thesis.Tests/Validators/CollectionComplexValidatorTest.cs
@@ -3,68 +3,75 @@ using System.ComponentModel.DataAnnotations;
 using Bachelor.Thesis.Benchmarking.CollectionComplex.Dto;
 using Light.GuardClauses;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Bachelor.Thesis.Tests.Validators;
 
 public class CollectionComplexValidatorTest
 {
-    public class CollectionFlatValidatorTest
+    public ITestOutputHelper Output { get; }
+
+    public CollectionComplexValidatorTest(ITestOutputHelper output)
     {
-        private readonly CollectionComplexDto _valid = CollectionComplexDto.ValidDto;
-        private readonly CollectionComplexDto _invalid = CollectionComplexDto.InvalidDto;
+        Output = output;
+    }
 
-        /*[Fact]
-        public void FluentValidatorValidDtoTest()
-        {
-            var fluentValidator = new FluentDtoValidator();
-            var result = fluentValidator.Validate(_valid);
+    private readonly CollectionComplexDto _valid = CollectionComplexDto.ValidDto;
+    private readonly CollectionComplexDto _invalid = CollectionComplexDto.InvalidDto;
 
-            result.IsValid.MustBe(true);
-        }
+    /*[Fact]
+    public void FluentValidatorValidDtoTest()
+    {
+        var fluentValidator = new FluentDtoValidator();
+        var result = fluentValidator.Validate(_valid);
 
-        [Fact]
-        public void LightValidatorValidDtoTest()
-        {
-            var lightValidator = new LightDtoValidator();
-            var result = lightValidator.Validate(_valid);
+        result.IsValid.MustBe(true);
+    }
 
-            result.IsValid.MustBe(true);
-        }*/
+    [Fact]
+    public void LightValidatorValidDtoTest()
+    {
+        var lightValidator = new LightDtoValidator();
+        var result = lightValidator.Validate(_valid);
 
-        [Fact]
-        public void ModelValidatorValidDtoTest()
-        {
-            var errors = new List<ValidationResult>();
-            var result = Validator.TryValidateObject(_valid, new ValidationContext(_valid), errors, true);
+        result.IsValid.MustBe(true);
+    }*/
 
-            result.MustBe(true);
-        }
+    [Fact]
+    public void ModelValidatorValidDtoTest()
+    {
+        var errors = new List<ValidationResult>();
+        var result = Validator.TryValidateObject(_valid, new ValidationContext(_valid), errors, true);
 
-        /*[Fact]
-        public void FluentValidatorInvalidDtoTest()
-        {
-            var fluentValidator = new FluentDtoValidator();
-            var result = fluentValidator.Validate(_invalid);
+        result.MustBe(true);
+    }
 
-            result.IsValid.MustBe(false);
-        }
+    /*[Fact]
+    public void FluentValidatorInvalidDtoTest()
+    {
+        var fluentValidator = new FluentDtoValidator();
+        var result = fluentValidator.Validate(_invalid);
 
-        [Fact]
-        public void LightValidatorInvalidDtoTest()
-        {
-            var lightValidator = new LightDtoValidator();
-            var result = lightValidator.Validate(_invalid);
+        result.IsValid.MustBe(false);
+    }
 
-            result.IsValid.MustBe(false);
-        }*/
+    [Fact]
+    public void LightValidatorInvalidDtoTest()
+    {
+        var lightValidator = new LightDtoValidator();
+        var result = lightValidator.Validate(_invalid);
 
-        [Fact]
-        public void ModelValidatorInvalidDtoTest()
-        {
-            var errors = new List<ValidationResult>();
-            var result = Validator.TryValidateObject(_invalid, new ValidationContext(_invalid), errors, true);
+        result.IsValid.MustBe(false);
+    }*/
 
-            result.MustBe(false);
-        }
+    [Fact]
+    public void ModelValidatorInvalidDtoTest()
+    {
+        var errors = new List<ValidationResult>();
+        var result = Validator.TryValidateObject(_invalid, new ValidationContext(_invalid), errors, true);
+
+        Output.WriteLine(Json.Serialize(errors));
+
+        result.MustBe(false);
     }
 }
---
+++ Code/Bachelor.Thesis.Tests/Validators/Json.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Bachelor.Thesis.Tests.Validators;
+
+public static class Json
+{
+    public static JsonSerializerOptions Options { get; } = new ()
+    {
+        WriteIndented = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
+    };
+
+    public static string Serialize(object value)
+    {
+        return JsonSerializer.Serialize(value, Options);
+    }
+}
