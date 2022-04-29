2e9645472cf675c52f6f0b7def6ef7e54cfc2bcd
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-04-29 12:20:14 +0200

    Updated wrongly called List

--- Code/Bachelor.Thesis.Benchmarking/CollectionComplex/ModelValidation/ValidateArticleInListAttribute.cs
+++ Code/Bachelor.Thesis.Benchmarking/CollectionComplex/ModelValidation/ValidateArticleInListAttribute.cs
@@ -7,16 +7,16 @@ public class ValidateArticleInListAttribute : ValidationAttribute
 {
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        var orderDetailsList = value as List<Article>;
+        var articleList = value as List<Article>;
 
-        if (orderDetailsList == null)
+        if (articleList == null)
             return ValidationResult.Success;
 
         var error = new List<ValidationResult>();
 
-        foreach (var order in orderDetailsList)
+        foreach (var article in articleList)
         {
-            Validator.TryValidateObject(order, new ValidationContext(order), error);
+            Validator.TryValidateObject(article, new ValidationContext(article), error);
             if (error.Count > 0)
                 return new ValidationResult(error.ToString());
         }
