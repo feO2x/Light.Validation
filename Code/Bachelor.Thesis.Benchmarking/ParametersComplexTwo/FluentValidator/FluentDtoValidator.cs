8870d7bccee4a71b0555a06802301af67f3e471c
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-04 12:29:15 +0200

    Refactored Import

--- Code/Bachelor.Thesis.Benchmarking/ParametersComplexTwo/FluentValidator/FluentDtoValidator.cs
+++ Code/Bachelor.Thesis.Benchmarking/ParametersComplexTwo/FluentValidator/FluentDtoValidator.cs
@@ -1,8 +1,9 @@
-﻿using FluentValidation;
+﻿using Bachelor.Thesis.Benchmarking.ParametersComplexTwo.Dto;
+using FluentValidation;
 
 namespace Bachelor.Thesis.Benchmarking.ParametersComplexTwo.FluentValidator;
 
-public class FluentDtoValidator : AbstractValidator<Dto.CustomerDto>
+public class FluentDtoValidator : AbstractValidator<CustomerDto>
 {
     public FluentDtoValidator()
     {
