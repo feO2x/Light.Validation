e1cfe7a3ae39a4307bbac8a17c7c6b3695aace09
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-04 13:37:51 +0200

    Changed fluentValidator to match previous commit

--- Code/Bachelor.Thesis.Benchmarking/CollectionComplex/FluentValidation/FluentOrderDetailsValidator.cs
+++ Code/Bachelor.Thesis.Benchmarking/CollectionComplex/FluentValidation/FluentOrderDetailsValidator.cs
@@ -11,6 +11,6 @@ public class FluentOrderDetailsValidator : AbstractValidator<OrderDetails>
         RuleFor(orderDetails => orderDetails.ProductId).NotEmpty().InclusiveBetween(10000, long.MaxValue);
         RuleFor(orderDetails => orderDetails.Date).NotEmpty().InclusiveBetween(new DateTime(2020, 01, 01), new DateTime(2022, 12, 31));
         RuleFor(orderDetails => orderDetails.Quantity).NotEmpty().InclusiveBetween(0, 1000);
-        RuleFor(orderDetails => orderDetails.PricePaid).NotEmpty().InclusiveBetween(0, 100000);
+        RuleFor(orderDetails => orderDetails.PricePaid).NotEmpty().InclusiveBetween((decimal) 0.01, (decimal) 100000.0);
     }
 }
