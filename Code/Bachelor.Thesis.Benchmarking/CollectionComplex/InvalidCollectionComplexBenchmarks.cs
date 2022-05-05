8b810e717be1bf0126d6bd71e24c922493a1701f
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-05 12:23:54 +0200

    Added Benchmarks for valid and invalid complex collection validation

---
+++ Code/Bachelor.Thesis.Benchmarking/CollectionComplex/InvalidCollectionComplexBenchmarks.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bachelor.Thesis.Benchmarking.CollectionComplex.Dto;
+using Bachelor.Thesis.Benchmarking.CollectionComplex.FluentValidation;
+using Bachelor.Thesis.Benchmarking.CollectionComplex.LightValidation;
+using BenchmarkDotNet.Attributes;
+
+namespace Bachelor.Thesis.Benchmarking.CollectionComplex;
+
+public class InvalidCollectionComplexBenchmarks
+{
+    public CollectionComplexDto Dto = CollectionComplexDto.InvalidDto;
+
+    public FluentDtoValidator FluentValidator = new();
+    public LightDtoValidator LightValidator = new();
+
+    [Benchmark(Baseline = true)]
+    public object? CheckViaLightValidator()
+    {
+        LightValidator.CheckForErrors(Dto, out var errors);
+        return errors;
+    }
+
+    [Benchmark]
+    public object? CheckViaFluentValidator() =>
+        FluentValidator.Validate(Dto);
+
+    [Benchmark]
+    public object CheckViaModelValidation()
+    {
+        var errors = new List<ValidationResult>();
+        Validator.TryValidateObject(Dto, new ValidationContext(Dto), errors, true);
+        return errors;
+    }
+}
